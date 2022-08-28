c62c04a4f67894419b82c6736add9b6b00c01b3e
vanessadixon1 <mclendonfamily88@gmail.com>
2022-08-28 17:35:18 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let player1DiceImg = document.querySelector('.img1');
+
+let player2DiceImg = document.querySelector('.img2');
+
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+let heading = document.querySelector('.container h1');
+
+player1DiceImg.setAttribute('src', './images/dice6.png');
+
+player2DiceImg.setAttribute('src', './images/dice6.png');
+
+player1DiceImg.setAttribute('src', './images/dice' + randomNumber1 + '.png');
+player2DiceImg.setAttribute('src', './images/dice' + randomNumber2 + '.png');
+
+if(randomNumber1 < randomNumber2) {
+    heading.textContent = "Player 2 Won!"
+} else if(randomNumber1 > randomNumber2) {
+    heading.textContent = "Player 1 Won!"
+} else {
+    heading.textContent = "Draw!"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(var i = 0; i < player1DiceImg.length; i++) {
+//     player1DiceImg[i].setAttribute('src',"./images/dice" +randomNumber1 + ".png")
+// }
